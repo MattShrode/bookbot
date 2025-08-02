3a4a1ffcd946270cf7fdf316fe2dd80549a322bd
Matt Shrode <mattshrode@gmail.com>
2025-08-01 23:42:37 -0500

    A: Read in file and print to console.

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    file_path = 'books/frankenstein.txt'
+
+    print(get_book_text(file_path))
+
+main()
