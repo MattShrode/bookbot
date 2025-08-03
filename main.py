c892a4bd4ceb3b43936d2a68dd3bec84d0f82b51
Matt Shrode <mattshrode@gmail.com>
2025-08-02 23:54:04 -0500

    B: Get file word count and print to console.

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ def get_book_text(file_path):
         file_contents = f.read()
     return file_contents
 
+def get_word_count(file_contents):
+    file_words = file_contents.split()
+    word_count = len(file_words)
+    return word_count
+
 def main():
     file_path = 'books/frankenstein.txt'
-
-    print(get_book_text(file_path))
+    file_contents = get_book_text(file_path)
+    word_count = get_word_count(file_contents)
+    print(f"{word_count} words found in the document")
 
 main()
