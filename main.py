e8b0d15eff16ea0d74ec3a027fc1f323e71f0f89
Matt Shrode <mattshrode@gmail.com>
2025-08-04 23:43:12 -0500

    D: Work on getting the character count.

--- main.py
+++ main.py
@@ -5,10 +5,20 @@ def get_book_text(file_path):
         file_contents = f.read()
     return file_contents
 
+def get_char_count(file_contents):
+    char_count = {}
+    for char in file_contents:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    print(char_count)
+
 def main():
     file_path = 'books/frankenstein.txt'
     file_contents = get_book_text(file_path)
     word_count = get_word_count(file_contents)
     print(f"{word_count} words found in the document")
+    get_char_count(file_contents)
 
 main()
