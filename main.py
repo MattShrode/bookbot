145954c79cd142e87d3d053c4a584d1c2ef5eb9c
Matt Shrode <mattshrode@gmail.com>
2025-08-07 22:54:12 -0500

    F: Refactor char function into the stats file.

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 from stats import get_word_count
+from stats import get_char_count
 
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
     return file_contents
 
-def get_char_count(file_contents):
-    file_contents_lower = file_contents.lower()
-    char_count = {}
-    for char in file_contents_lower:
-        if char in char_count:
-            char_count[char] += 1
-        else:
-            char_count[char] = 1
-    print(char_count)
+
 
 def main():
     file_path = 'books/frankenstein.txt'
     file_contents = get_book_text(file_path)
     word_count = get_word_count(file_contents)
     print(f"{word_count} words found in the document")
-    get_char_count(file_contents)
+    char_count = get_char_count(file_contents)
+    print(char_count)
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,13 @@ def get_word_count(file_contents):
     file_words = file_contents.split()
     word_count = len(file_words)
     return word_count
+
+def get_char_count(file_contents):
+    file_contents_lower = file_contents.lower()
+    char_count = {}
+    for char in file_contents_lower:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
