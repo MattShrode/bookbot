ba504481d9b9d59721f8eb19e43e93c14dff13ea
Matt Shrode <mattshrode@gmail.com>
2025-08-08 23:43:46 -0500

    G: Convert char dictionary into a list of dictionary pairs.

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ def get_book_text(file_path):
         file_contents = f.read()
     return file_contents
 
-
+def get_list_of_dict(char_count_dict):
+    list_of_char_count = [{"char": key, "num": value} for key, value in char_count_dict.items()]
+    return list_of_char_count
 
 def main():
     file_path = 'books/frankenstein.txt'
@@ -14,6 +16,7 @@ def main():
     word_count = get_word_count(file_contents)
     print(f"{word_count} words found in the document")
     char_count = get_char_count(file_contents)
-    print(char_count)
+    list_of_char_count = get_list_of_dict(char_count)
+    print(list_of_char_count)
 
 main()
