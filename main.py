e2749742ee89c2eedc4e54ebd351d8d6bd98dfcd
Matt Shrode <mattshrode@gmail.com>
2025-08-09 23:31:14 -0500

    H: Extract alphabetical characters and their values into a sorted list.

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ def get_list_of_dict(char_count_dict):
     list_of_char_count = [{"char": key, "num": value} for key, value in char_count_dict.items()]
     return list_of_char_count
 
+def sort_on(items):
+    return items["num"]
+
+def print_report(sorted_list_of_char_count):
+    for dict in sorted_list_of_char_count:
+        if dict["char"].isalpha():
+            print(f"{dict['char']}: {dict['num']}")
+        
+
 def main():
     file_path = 'books/frankenstein.txt'
     file_contents = get_book_text(file_path)
@@ -17,6 +26,8 @@ def main():
     print(f"{word_count} words found in the document")
     char_count = get_char_count(file_contents)
     list_of_char_count = get_list_of_dict(char_count)
-    print(list_of_char_count)
+    list_of_char_count.sort(reverse=True, key=sort_on)
+    #print(list_of_char_count)
+    print_report(list_of_char_count)
 
 main()
